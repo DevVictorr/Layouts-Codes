9b387a8e28fef2caa98cc7b649cfe448274dbba6
Victor Antunes <ioursva@gmail.com>
2020-10-08 15:36:52 -0300

    Comments update

--- Layouts/Projeto1/js/script.js
+++ Layouts/Projeto1/js/script.js
@@ -38,17 +38,17 @@ alert("error!!! - Senha   Digite: aaa");
 //ABAS DO MENU LATERAL//
 function aba1(){
 
-
+    //troca do main
     document.getElementById("main").innerHTML='<div id="caixa-s"><h1>Bem-Vindo</h1><h2>V.A Store</h1><h3>Sempre com você</h2></div>  '
     
-
+    //Estilos - Menu
     document.getElementById("m-home").style.backgroundColor = "#363636";
     document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
-
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#fff";
     document.getElementById("m-item").style.color ="#000";
     document.getElementById("m-cadastro").style.color ="#000";
@@ -58,17 +58,17 @@ function aba1(){
 }
 
 function aba2(){
-
+//troca do main
     document.getElementById("main").innerHTML='<div class="produtos"><div class="produto-f"> <img src="./img/camisa1.png" alt="test"><h1>Dior</h1> <br><p class="no-stock">fora de estoque</p></div><div class="produto-f"> <img src="./img/gucci1.jpg" alt="test"><h1>acquard</h1><br><p>R$9.410</p></div><div class="produto-f"> <img src="./img/3.jpg" alt="test"><h1>Atemporal</h1> <br><p>R$4.050</p></div>'
    
-   
+   //Estilos - menu 
     document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-item").style.backgroundColor = "#363636";
     document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
     document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
-
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#000";
     document.getElementById("m-item").style.color ="#fff";
     document.getElementById("m-cadastro").style.color ="#000";
@@ -78,17 +78,17 @@ function aba2(){
 }
 
 function aba3(){
-
+//troca do main
 document.getElementById("main").innerHTML = '<form action="#"><label for="">Nome</label> <input id="nome3"  type="text" placeholder="Seu Nome" value="" ><br><label for="">Senha</label> <input required id="senha3"  type="password" placeholder="Senha" value="" ><br><label for="">E-mail</label> <input required id="email3" type="email" placeholder="Email" value="" ><br><input type="submit" onclick="cadastro()" value="Cadastrar"></form>'
 
-
-document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-cadastro").style.backgroundColor = "#363636";
-document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
-
+//Estilos - Menu
+    document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-cadastro").style.backgroundColor = "#363636";
+    document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#000";
     document.getElementById("m-item").style.color ="#000";
     document.getElementById("m-cadastro").style.color ="#fff";
@@ -98,17 +98,17 @@ document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
 }
 
 function aba4(){
-
+//troca do main
 document.getElementById("main").innerHTML = '<form action="#"><label for="">Nome</label> <input required  id="nome4"  type="text" placeholder="Seu Nome" value="" ><br><label for="">E-mail</label> <input required  id="email4" type="email" placeholder="Email" value="" ><br><label for="">Texto: </label><textarea required id="txt4" cols="30" rows="10"></textarea><br><input type="submit" onclick="enviarformulario()" value="Enviar Mensagem"></form>'
 
-
-document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-contato").style.backgroundColor = "#363636";
-document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
-
+//Estilos - Menu
+    document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-contato").style.backgroundColor = "#363636";
+    document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#000";
     document.getElementById("m-item").style.color ="#000";
     document.getElementById("m-cadastro").style.color ="#000";
@@ -119,16 +119,17 @@ document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
 
 
 function aba5(){
-document.getElementById("main").innerHTML = '<h1>Um pouco sobre Dior</h1><a href="http://www.youtube.com/embed/fb968UyAPDY" target="blank"><i class="fab fa-youtube"> Clique aqui para ir ao nosso video</i></a> '
+    //troca do main
+    document.getElementById("main").innerHTML = '<h1>Um pouco sobre Dior</h1><a href="http://www.youtube.com/embed/fb968UyAPDY" target="blank"><i class="fab fa-youtube"> Clique aqui para ir ao nosso video</i></a> '
 
-
-document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-sobre").style.backgroundColor = "#363636";
-document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
-
+//Estilos - Menu
+    document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-sobre").style.backgroundColor = "#363636";
+    document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#000";
     document.getElementById("m-item").style.color ="#000";
     document.getElementById("m-cadastro").style.color ="#000";
@@ -138,17 +139,17 @@ document.getElementById("m-equipe").style.backgroundColor = "#F1F1F1";
 }
 
 function aba6(){
+//troca do main
+    document.getElementById("main").innerHTML = '<div class="equipe-f"><img src="./img/avatar.png" alt="test"><br><br><h1>Victor A.</h1> <br><h4>entusiasta javascript, apaixonado por clean code e boas práticas<br> está sempre interessado em adquirir novos conhecimentos</h4><a href="https://github.com/DevVictorr" target="_blank"><i class="fab fa-github"></i> Github</a> <br><a href="https://www.linkedin.com/in/victordsantunes/"target="_blank"><i class="fab fa-linkedin-in"></i></i> Linkedin</a></div>'
 
-document.getElementById("main").innerHTML = '<div class="equipe-f"><img src="./img/avatar.png" alt="test"><br><br><h1>Victor A.</h1> <br><h4>entusiasta javascript, apaixonado por clean code e boas práticas<br> está sempre interessado em adquirir novos conhecimentos</h4><a href="https://github.com/DevVictorr" target="_blank"><i class="fab fa-github"></i> Github</a> <br><a href="https://www.linkedin.com/in/victordsantunes/"target="_blank"><i class="fab fa-linkedin-in"></i></i> Linkedin</a></div>'
-
-
-document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
-document.getElementById("m-equipe").style.backgroundColor = "#363636";
-
+//Estilos - Menu
+    document.getElementById("m-home").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-item").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-cadastro").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-contato").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-sobre").style.backgroundColor = "#F1F1F1";
+    document.getElementById("m-equipe").style.backgroundColor = "#363636";
+//Estilos - Menu
     document.getElementById("m-home").style.color ="#000";
     document.getElementById("m-item").style.color ="#000";
     document.getElementById("m-cadastro").style.color ="#000";
